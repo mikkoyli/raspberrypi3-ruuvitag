ca64cfef6657471e0d1c6b29827e68b9ef253245
Mikko Yliniemi <mikessu@iki.fi>
2018-04-25 10:43:37 +0300

    connection check script

---
+++ check-network.py
@@ -0,0 +1,12 @@
+import urllib2
+
+def internet_on():
+    try:
+        urllib2.urlopen('http://www.google.com/', timeout=1)
+	print("Establish connection succesfull")
+        return True
+    except urllib2.URLError as err: 
+	print("Could not establish connection")
+        return False
+
+internet_on()
